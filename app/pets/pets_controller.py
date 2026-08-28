694d80d727fff9103102233717d0430c68a42df9
Gonzalo Albornoz <galbornoz2026@alu.uct.cl>
2026-08-28 08:45:48 -0400

    Integra ApiResponse y corrige eliminacion de mascotas

--- app/pets/pets_controller.py
+++ app/pets/pets_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.pets.pets_schemas import CreatePetDto, Pet, UpdatePetDto
+from app.shared import ApiResponse
+from app.pets.pets_schemas import CreatePetDto, UpdatePetDto
 from app.pets.pets_service import pets_service
 
 router = APIRouter(
@@ -11,31 +12,40 @@ router = APIRouter(
 @router.get("")
 def find_all(studentId: str):
     pets = pets_service.find_all_for_student(studentId)
-    return {
-        "message": "Lista de mascotas recuperada",
-        "data": pets
-    }
+    return ApiResponse(
+        success=True,
+        message="Lista de mascotas recuperada",
+        data=pets,
+        error=None,
+    )
 
 @router.post("", status_code=201)
 def create(studentId: str, body: CreatePetDto):
     new_pet = pets_service.create(studentId, body)
-    return {
-        "message": "Mascota creada con éxito",
-        "data": new_pet
-    }
+    return ApiResponse(
+        success=True,
+        message="Mascota creada con éxito",
+        data=new_pet,
+        error=None,
+    )
 
 @router.patch("/{petId}")
 def update(studentId: str, petId: str, body: UpdatePetDto):
     updated_pet = pets_service.update(studentId, petId, body)
-    return {
-        "message": "Mascota actualizada con éxito",
-        "data": updated_pet
-    }
+    return ApiResponse(
+        success=True,
+        message="Mascota actualizada con éxito",
+        data=updated_pet,
+        error=None,
+    )
 
 @router.delete("/{petId}")
 def delete(studentId: str, petId: str):
     deleted_pet = pets_service.delete(studentId, petId)
-    return {
-        "message": "Mascota eliminada con éxito",
-        "data": deleted_pet
-    }
+    return ApiResponse(
+        success=True,
+        message="Mascota eliminada con éxito",
+        data=deleted_pet,
+        error=None,
+    )
+    
